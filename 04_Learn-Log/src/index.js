bbd223d211e28965075820d239fae3b1bd7627f1
cavon <425247833@qq.com>
2024-02-02 20:42:15 +0800

    feat：事件，事件的默认行为和事件冒泡

--- 04_Learn-Log/src/index.js
+++ 04_Learn-Log/src/index.js
@@ -1,4 +1,11 @@
+/*
+ * @Author: cavon 425247833@qq.com
+ * @Date: 2024-02-02 18:29:29
+ * @LastEditors: cavon 425247833@qq.com
+ * @LastEditTime: 2024-02-02 19:32:04
+ * @FilePath: /react-course/04_Learn-Log/src/index.js
+ */
 import ReactDOM from "react-dom/client";
-import App from './App.js';
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
