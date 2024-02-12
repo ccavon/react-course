5d6c795686522331b0c83ac1ad4dc4ace7a7b83a
cavon <425247833@qq.com>
2024-02-12 19:44:55 +0800

    feat：添加Card组件，将样式一样的整成公共组件，方便在维护的时候只查找一个组件

--- 04_Learn-Log/src/App.js
+++ 04_Learn-Log/src/App.js
@@ -1,23 +1,19 @@
 /*
  * @Author: cavon 425247833@qq.com
- * @Date: 2024-02-11 16:58:37
+ * @Date: 2024-02-12 17:42:26
  * @LastEditors: cavon 425247833@qq.com
- * @LastEditTime: 2024-02-11 22:07:52
+ * @LastEditTime: 2024-02-12 19:26:11
  * @FilePath: /react-course/04_Learn-Log/src/App.js
- * @Description: 类组件
+ * @Description: 主页入口
  */
-import React, { Component } from 'react';
-import './App.css';
-import User from './components/User';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <User name={'猪八戒'} age={28} gender={'男'} />
-      </div>
-    )
-  }
+import Logs from './Components/Logs/Logs';
+import './index.css';
+
+const App = () => {
+  return <div>
+    <Logs />
+  </div>;
 }
 
 export default App;
