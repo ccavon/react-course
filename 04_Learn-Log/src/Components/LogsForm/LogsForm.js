bbe2a2a412a01917cbc1f9d8ae5d541f2dfe5653
cavon <425247833@qq.com>
2024-02-15 23:36:35 +0800

    feat：获取表单数据，将表单项的每个数据都汇总到一个数据中用来提交表单

--- 04_Learn-Log/src/Components/LogsForm/LogsForm.js
+++ 04_Learn-Log/src/Components/LogsForm/LogsForm.js
@@ -2,33 +2,89 @@
  * @Author: cavon 425247833@qq.com
  * @Date: 2024-02-12 19:47:48
  * @LastEditors: cavon 425247833@qq.com
- * @LastEditTime: 2024-02-12 21:28:46
+ * @LastEditTime: 2024-02-15 23:34:58
  * @FilePath: /react-course/04_Learn-Log/src/Components/LogsForm/LogsForm.js
  * @Description: 表单添加组件
  */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import Card from '../UI/Card/Card';
 import './LogsForm.css';
 
 const LogsForm = () => {
+  /**
+      当表单项发生变化时，获取用户输入的内容
+
+      获取到当前触发事件的对象
+      事件对象中保存了当前事件触发的所有信息，event.target执行的是触发事件的对象（DOM对象）
+
+      在React中，通常表单不需要自行提交，而是要通过React提交
+   */
+
+  const descRef = useRef();
+
+  // 创建三个变量，用来存储表单中的数据
+  let inputDate = '';
+  let inputDesc = '';
+  let inputTime = 0;
+
+  // 创建一个响应函数，监听表单项日期的变化
+  const dateChangeHandler = (e) => {
+    console.log(e.target.value);
+    inputDate = e.target.value;
+  }
+
+  // 创建一个响应函数，监听表单项内容的变化
+  const descChangeHandler = (e) => {
+    console.log('desc发生变化了');
+    // const desc = document.getElementById('desc');
+    // console.log(desc.value);
+
+    // console.log(descRef.current.value);
+
+    console.log(e.target.value);
+    inputDesc = e.target.value;
+  }
+
+  // 创建一个响应函数，监听表单项时长的变化
+  const timeChangeHandler = (e) => {
+    console.log(e.target.value);
+    inputTime = e.target.value;
+  }
+
+  // 当表单提交时，汇总表单中的数据
+  const formSubmitHandler = (e) => {
+    e.preventDefault(); // 取消表单的默认行为
+    console.log('表单提交了');
+
+    // 获取表单项中的数据日期、内容、时长
+    // console.log(inputDate, inputDesc, inputTime);
+    // 将数据拼装为一个对象
+    const newLog = {
+      date: new Date(inputDate),
+      desc: inputDesc,
+      time: +inputTime // 隐式转换为number
+    };
+    console.log(newLog);
+  }
+
   return (
     <Card className="logs-from">
-      <form>
+      <form onSubmit={formSubmitHandler}>
 
         <div className="form-item">
           <label htmlFor="date">日期</label>
-          <input type="date" id="date" />
+          <input type="date" id="date" onChange={dateChangeHandler} />
         </div>
 
         <div className="form-item">
           <label htmlFor="content">内容</label>
-          <input type="text" id="content" />
+          <input type="text" id="desc" onChange={descChangeHandler} ref={descRef} />
         </div>
 
         <div className="form-item">
           <label htmlFor="time">时长</label>
-          <input type="number" id="time" />
+          <input type="number" id="time" onChange={timeChangeHandler} />
         </div>
 
         <div className="form-btn">
