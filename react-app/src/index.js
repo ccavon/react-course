6bf6e42912ed2f32d154f61420b22c6ad6c2dfc6
cavon <425247833@qq.com>
2024-02-19 21:07:31 +0800

    feat：创建React项目（自动）

---
+++ react-app/src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: cavon 425247833@qq.com
+ * @Date: 2024-02-19 20:27:42
+ * @LastEditors: cavon 425247833@qq.com
+ * @LastEditTime: 2024-02-19 21:02:34
+ * @FilePath: /react-course/react-app/src/index.js
+ * @Description: JavaScript入口
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
