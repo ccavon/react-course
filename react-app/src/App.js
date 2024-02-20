bcb3486fcc024122e1f227b0ef51b3c7145bc9f0
cavon <425247833@qq.com>
2024-02-20 22:05:53 +0800

    feat：内联样式和样式表

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,15 +2,31 @@
  * @Author: cavon 425247833@qq.com
  * @Date: 2024-02-19 21:01:17
  * @LastEditors: cavon 425247833@qq.com
- * @LastEditTime: 2024-02-19 21:07:07
+ * @LastEditTime: 2024-02-20 21:50:20
  * @FilePath: /react-course/react-app/src/App.js
  */
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
+
+  const [redBorder, setRedBorder] = useState(true);
+
+  // const pStyle = {
+  //   color: "red",
+  //   background: "#bfa",
+  //   border: redBorder ? "4px solid red" : "4px solid blue"
+  // };
+
+  const clickHandler = () => {
+    setRedBorder(false);
+  }
+
   return (
     <div>
-      我是App
+      {/* <p style={pStyle}>我是一个段落</p> */}
+      <p className={`p1 ${redBorder ? "" : "buleBorder"}`}>我是一个段落</p>
+      <button onClick={clickHandler}>点我一下</button>
     </div>
   );
 };
