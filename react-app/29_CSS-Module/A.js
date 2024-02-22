1e386069ab5443207dfe390ab3962bb2a0399bec
cavon <425247833@qq.com>
2024-02-22 22:12:23 +0800

    feat：使用CSS Module，解决项目中类名冲突的问题

---
+++ react-app/29_CSS-Module/A.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: cavon 425247833@qq.com
+ * @Date: 2024-02-22 21:59:28
+ * @LastEditors: cavon 425247833@qq.com
+ * @LastEditTime: 2024-02-22 22:01:53
+ * @FilePath: /react-course/react-app/src/A.js
+ * @Description: 
+ */
+import React from "react";
+import classes from "./A.module.css";
+
+const A = () => {
+  return (
+    <div>
+      <p className={classes.p1}>我是A组件</p>
+    </div>
+  );
+};
+
+export default A;
