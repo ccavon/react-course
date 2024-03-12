aace1e0f837a50c65b2e663607a986d420994be7
cavon <425247833@qq.com>
2024-03-12 19:21:19 +0800

    refactor：项目准备更改结构，以防多余文件

---
+++ react-app/31_项目准备/App.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: cavon 425247833@qq.com
+ * @Date: 2024-02-19 21:01:17
+ * @LastEditors: cavon 425247833@qq.com
+ * @LastEditTime: 2024-03-12 19:12:06
+ * @FilePath: /react-course/react-app/src/App.js
+ */
+
+const App = () => {
+
+  return (
+    <div style={{  width: '750rem', height: 200, backgroundColor: '#bfa'}}>我是App</div>
+  );
+};
+
+export default App;
